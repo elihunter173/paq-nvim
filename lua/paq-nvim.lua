7e47f7c84178ee42897091867bc316514221f526
Eli W. Hunter <elihunter173@gmail.com>
2020-12-24 22:51:14 -0500

    Implement local plugins
    This removes the `url` option in paq and instead adds an `as` option so
    that that can be used to specify the destination name

--- lua/paq-nvim.lua
+++ lua/paq-nvim.lua
@@ -1,40 +1,37 @@
--- Constants
-local PATH    = vim.fn.stdpath('data') .. '/site/pack/paqs/'
-local GITHUB  = 'https://github.com/'
-local REPO_RE = '^[%w-]+/([%w-_.]+)$'
-
 local uv = vim.loop -- Alias for Neovim's event loop (libuv)
-local packages = {} -- Table of 'name':{options} pairs
-local call_cmd      -- To handle mutual funtion recursion (Lua shenanigans)
+
+local PATH = vim.fn.stdpath("data") .. "/site/pack/paqs/"
+
+local packages = {} -- Table of 'name':{plugin} pairs
 
 local function print_res(action, args, ok)
-    local res = ok and 'Paq: ' or 'Paq: Failed to '
-    print(res .. action .. ' ' .. args)
+    local res = ok and "Paq: " or "Paq: Failed to "
+    print(res .. action .. " " .. args)
 end
 
+local call_cmd -- To handle mutual funtion recursion (Lua shenanigans)
 local function run_hook(hook, name, dir)
     local t = type(hook)
-    if t == 'function' then
-        vim.cmd('packloadall!')
+    if t == "function" then
+        vim.cmd("packloadall!")
         local ok = pcall(hook)
-        print_res('run hook for', name, ok) -- FIXME: How to print an interned string?
+        print_res("run hook for", name, ok) -- FIXME: How to print an interned string?
 
-    elseif t == 'string' then
-        local hook_args = {}
-        local hook_cmd
+    elseif t == "string" then
+        local hook_cmd = {}
         for word in hook:gmatch("%S+") do
-            table.insert(hook_args, word)
+            table.insert(hook_cmd, word)
         end
-        hook_cmd = table.remove(hook_args, 1)
-        call_cmd(hook_cmd, name, dir, "run `"..hook.."` for", hook_args)
+        call_cmd(hook_cmd, name, dir, "run `"..hook.."` for")
     end
 end
 
-function call_cmd(cmd, name, dir, action, args, hook)
+function call_cmd(cmd, name, dir, action, hook)
+    local path, args = table.remove(cmd, 1), cmd
     local handle
     handle =
-        uv.spawn(cmd,
-            {args=args, cwd=(action ~= 'install' and dir or nil)}, -- FIXME: Handle install case better
+        uv.spawn(path,
+            {args=args, cwd=(action ~= "install" and dir or nil)}, -- FIXME: Handle install case better
             vim.schedule_wrap(
                 function(code)
                     print_res(action, name, code == 0)
@@ -45,30 +42,49 @@ function call_cmd(cmd, name, dir, action, args, hook)
         )
 end
 
-local function install_pkg(name, dir, isdir, args)
-    local install_args
-    if not isdir then
-        if args.branch then
-            install_args = {'clone', args.url, '-b',  args.branch, '--single-branch', dir}
+local function install_pkg(name, dir, isdir, plugin)
+    -- Plugin already installed
+    if isdir then return end
+
+    if plugin.type == "git" then
+        local cmd
+        if plugin.branch then
+            cmd = {"git", "clone", plugin.url, "-b",  plugin.branch, "--single-branch", dir}
         else
-            install_args = {'clone', args.url, dir}
+            cmd = {"git", "clone", plugin.url, dir}
         end
-        call_cmd('git', name, dir, 'install', install_args, args.hook)
+        call_cmd(cmd, name, dir, "install", plugin.hook)
+
+    elseif plugin.type == "local" then
+        local cmd
+        if vim.fn.executable("ln") then
+            cmd = {"ln", "-sf", plugin.url, dir}
+        elseif jit.os == "windows" then
+            cmd = {"cmd", "/C", "mklink", "/d", plugin.url, dir}
+        else
+            print_res("install", "no executable symlink command found", false)
+            return
+        end
+        call_cmd(cmd, name, dir, "install", plugin.hook)
     end
 end
 
-local function update_pkg(name, dir, isdir, args)
-    if isdir then
-        call_cmd('git', name, dir, 'update', {'pull'}, args.hook)
+local function update_pkg(name, dir, isdir, plugin)
+    -- Plugin already installed
+    if not isdir then return end
+
+    if plugin.type == "git" then
+        call_cmd({"git", "pull"}, name, dir, "update", plugin.hook)
+    elseif plugin.type == "local" then
+        -- Local plugins are always up to date
     end
 end
 
 local function map_pkgs(fn)
-    local dir, isdir
-    for name, args in pairs(packages) do
-        dir = PATH .. (args.opt and 'opt/' or 'start/') .. name
-        isdir = vim.fn.isdirectory(dir) ~= 0
-        fn(name, dir, isdir, args)
+    for name, plugin in pairs(packages) do
+        local dir = PATH .. (plugin.opt and "opt/" or "start/") .. name
+        local isdir = vim.fn.isdirectory(dir) ~= 0
+        fn(name, dir, isdir, plugin)
     end
 end
 
@@ -78,16 +94,16 @@ local function rmdir(dir, ispkgdir)
     while handle do
         name, t = uv.fs_scandir_next(handle)
         if not name then break end
-        child = dir .. '/' .. name
-        if ispkgdir then --check which packages are listed
-            if packages[name] then --do nothing
+        child = dir .. "/" .. name
+        if ispkgdir then -- check which packages are listed
+            if packages[name] then -- do nothing
                 ok = true
-            else --package isn't listed, remove it
+            else -- package isn't listed, remove it
                 ok = rmdir(child)
-                print_res('uninstall', name, ok)
+                print_res("uninstall", name, ok)
             end
-        else --it's an arbitrary directory or file
-            ok = (t == 'directory') and rmdir(child) or uv.fs_unlink(child)
+        else -- it's an arbitrary directory or file
+            ok = (t == "directory") and rmdir(child) or uv.fs_unlink(child)
         end
         if not ok then return end
     end
@@ -95,27 +111,44 @@ local function rmdir(dir, ispkgdir)
 end
 
 local function paq(args)
-    if type(args) == 'string' then
-        args = {args}
+    local path = type(args) == "string" and args or args[1]
+    -- vim.fn.isdirectory doesn't recognize ~
+    path = path:gsub("^~", os.getenv("HOME"))
+
+    local plugin = {
+        hook = args.hook,
+        opt = args.opt,
+        branch = args.branch,
+    }
+    if vim.fn.isdirectory(path) ~= 0 then
+        plugin.name = args.as or path:match("^.+/(.+)$")
+        plugin.type = "local"
+        plugin.url = path
+    elseif path:match("^[%w-]+/[%w-_.]+$") then
+        plugin.name = args.as or path:match("^[%w-]+/([%w-_.]+)$")
+        plugin.type = "git"
+        plugin.url = "https://github.com/" .. path .. ".git"
+    else
+        plugin.name = args.as
+        plugin.type = "git"
+        plugin.url = path
     end
 
-    local reponame = args[1]:match(REPO_RE)
-    if not reponame then
-        print_res('parse', args[1])
+    if not plugin.name then
+        print_res("parse", path)
+        return
+    end
+    if plugin.branch ~= nil and plugin.type ~= "git" then
+        print_res("parse", "`branch` only valid for git plugins")
         return
     end
 
-    packages[reponame] = {
-        branch = args.branch,
-        hook   = args.hook,
-        opt    = args.opt,
-        url    = args.url or GITHUB .. args[1] .. '.git',
-    }
+    packages[plugin.name] = plugin
 end
 
 return {
     install = function() map_pkgs(install_pkg) end,
     update  = function() map_pkgs(update_pkg) end,
-    clean   = function() rmdir(PATH..'start', 1); rmdir(PATH..'opt', 1) end,
+    clean   = function() rmdir(PATH.."start", true); rmdir(PATH.."opt", true) end,
     paq     = paq
 }
